73b0c5f3b0d4330061ef37d673e059dc69d5ecfc
Milagros LC <evidal0710@ich.edu.pe>
2022-01-31 16:27:08 -0500

    Add config environments

--- coffee-queen/src/data/services/api/productos.service.ts
+++ coffee-queen/src/data/services/api/productos.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { catchError, map } from 'rxjs';
+import { Apiclass } from 'src/data/schema/ApiClass.class';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class ProductosService {
-
-  constructor() { }
+export class ProductosService extends Apiclass {
+  getAllProducts(): Observable<{
+    error: boolean;
+    msg: string;
+    data: Product[];
+  }> {
+    const response = { error: false, msg: '', data: null };
+    return this.http.get<Product[]>(this.url + 'productos').pipe(
+      map( res => {
+        response.data = res;
+        return response;
+      }),
+      catchError(this.error)
+    );
+  }
 }
--- coffee-queen/src/environments/environment.dev.ts
+++ coffee-queen/src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  uri: 'http://localhost:4200/'
+  uri: 'http://localhost:3000/'
 };
