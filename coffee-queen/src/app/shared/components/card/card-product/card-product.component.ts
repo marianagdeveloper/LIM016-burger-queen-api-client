011bd1bb428e06f41c8224538d92b4f13a03151f
Milagros LC <evidal0710@ich.edu.pe>
2022-02-09 17:04:48 -0500

    add buttons + and - in products

--- coffee-queen/src/app/shared/components/card/card-product/card-product.component.ts
+++ coffee-queen/src/app/shared/components/card/card-product/card-product.component.ts
@@ -2,46 +2,46 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ICardProduct } from './card-product.metadata';
 import { ProductService } from 'src/app/data/services/api/product.service';
 interface Pedido {
-  item: string,
-  cantidad: number,
-  precio: number,
-  stock: number,
+  item: string;
+  cantidad: number;
+  precio: number;
+  stock: number;
 }
 interface Pedido1 {
-  name: string,
-  price: number,
-  image: string,
-  type: string,
+  name: string;
+  price: number;
+  image: string;
+  type: string;
 }
 
 @Component({
   selector: 'app-card-product',
   templateUrl: './card-product.component.html',
-  styleUrls: ['./card-product.component.scss']
+  styleUrls: ['./card-product.component.scss'],
 })
 export class CardProductComponent implements OnInit {
   @Input() data!: ICardProduct;
 
-/*   public pedidos: Pedido1={
+  /*   public pedidos: Pedido1={
     name: '',
     price: 0,
     image: '',
     type: '',
   } */
-
-  public selectProduct?:any= [];
-  constructor(public productService: ProductService ) { }
+  public activeDiv: any;
+  public desActiveDiv: any;
+  public selectProduct?: any = [];
+  constructor(public productService: ProductService) {}
 
   ngOnInit(): void {
-
+    //this.activeDiv=''
   }
 
-  getInfoProduct(){
+  getInfoProduct() {
+    // this.productService.disparadorProduct.next(this.selectProduct)
+    this.productService.setProducts(this.data);
 
-  
-   // this.productService.disparadorProduct.next(this.selectProduct)
-   this.productService.setProducts(this.data);
-
-
+    this.activeDiv = 'true';
+    this.desActiveDiv = 'disableDiv';
   }
 }
