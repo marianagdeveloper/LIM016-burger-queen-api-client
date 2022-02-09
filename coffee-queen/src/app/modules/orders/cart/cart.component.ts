07765cb31781f872a8f3f766cfe01de92e8bb147
Milagros LC <evidal0710@ich.edu.pe>
2022-02-08 22:59:12 -0500

    try to clean order

--- coffee-queen/src/app/modules/orders/cart/cart.component.ts
+++ coffee-queen/src/app/modules/orders/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/data/services/api/product.service';
+import { ICardProduct } from 'src/app/shared/components/card/card-product/card-product.metadata';
 
 interface orderProducts {
   id: number;
@@ -27,7 +28,7 @@ interface Order {
   styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  public products: any;
+  public products!: ICardProduct[];
   public order: Order = {
     _id: 0,
     userId: 0,
@@ -58,22 +59,37 @@ export class CartComponent implements OnInit {
   public nameProduct: string = '';
 
   constructor(public productService: ProductService) {}
-
   ngOnInit(): void {
+    this.arrayNew = this.productService.newArrayProducts;
     this.products = this.productService.arrayProducts;
-
+    this.productsEliminados = this.productService.eliminadosProducts;
+    console.log(this.productsEliminados);
+    if (this.arrayNew.length === 0) {
+      this.products = this.productService.arrayProducts;
+      console.log('product si esta vacio arraynew', this.products);
+    } else {
+      console.log('antes de push', this.products);
+      this.products.filter((productos: any) => {
+        this.productsEliminados.forEach((ele: any) => {
+          if (productos.name !== ele.name) {
+            console.log('entraste', productos.name);
+            this.nuevisimoArray.push(productos);
+          }
+        });
+      });
+      this.products = this.nuevisimoArray;
+      console.log('despues de push', this.products);
+    }
     this.products.map((ele: any) => {
       this.order.total += ele.price;
     });
   }
-
   aumentarCantidad(product: any) {
     this.cant = product.qty += 1;
     product.subTotal = this.cant * product.price;
     this.order.total += product.subTotal;
     this.order.total -= product.price * (this.cant - 1);
   }
-
   disminuirCantidad(product: any) {
     if (product.qty < 2) {
       product.qty = 1;
@@ -87,16 +103,23 @@ export class CartComponent implements OnInit {
 
     return this.order.total;
   }
+  public arrayNew: ICardProduct[] = [];
+  public eliminados: ICardProduct[] = [];
+  public productsEliminados: ICardProduct[] = [];
+  public nuevisimoArray: ICardProduct[] = [];
   deleteProduct(product: any) {
-    let arrayNew: any[] = [];
     this.products.filter((item: any) => {
       if (item.name !== product.name) {
-        arrayNew.push(item);
+        this.arrayNew.push(item);
       } else {
+        this.eliminados.push(item);
         this.deleteSubtotal = product.subTotal - product.price;
       }
     });
-    this.products = arrayNew;
+    this.products = this.arrayNew;
+    this.productService.setProducts(this.arrayNew[0]);
+  /*   this.productService.setNewProducts(this.arrayNew[0]);
+    this.productService.setEliminadosProducts(this.eliminados[0]); */
     if (product.qty > 1) {
       this.order.total = this.disminuirCantidad(product) - this.deleteSubtotal;
     } else {
