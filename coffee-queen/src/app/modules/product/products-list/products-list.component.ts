b607a0b0224fa198ffd4dd5858b0566c0910e394
Milagros LC <evidal0710@ich.edu.pe>
2022-02-11 17:20:05 -0500

    add funtionality and style to search button

--- coffee-queen/src/app/modules/product/products-list/products-list.component.ts
+++ coffee-queen/src/app/modules/product/products-list/products-list.component.ts
@@ -3,20 +3,40 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../../data/services/api/product.service';
 import { ICardProduct } from '../../../shared/components/card/card-product/card-product.metadata';
 
+//@Pipe({filter:'filter'})
+
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.scss']
+  styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent implements OnInit {
-
   public products?: ICardProduct[];
+  public getProduct: string = '';
+  public array: any;
+
   constructor(public productService: ProductService) {}
 
   ngOnInit() {
-    this.productService.getAllProducts().subscribe(data => {
+    this.cleanSearch();
+  }
+  searchProduct() {
+    console.log('entrastre');
+    this.productService.getAllProducts().subscribe((data) => {
+      if(this.getProduct===""){
+        this.products = data;
+      }else{
       this.products = data;
+      this.products = this.products?.filter(
+        (elem) => elem.name.toLowerCase().indexOf(this.getProduct) > -1
+      );
+    }
     });
   }
-
+  cleanSearch(){
+    this.productService.getAllProducts().subscribe((data) => {
+      this.products = data;
+    });
+    this.getProduct='';
+  }
 }
