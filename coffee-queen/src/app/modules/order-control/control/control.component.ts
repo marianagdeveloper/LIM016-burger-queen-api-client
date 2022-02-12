8d4d76bee88a286cc15503d7b049ad3ca463c531
Milagros LC <evidal0710@ich.edu.pe>
2022-02-11 20:12:30 -0500

    add styles to order-control

--- coffee-queen/src/app/modules/order-control/control/control.component.ts
+++ coffee-queen/src/app/modules/order-control/control/control.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./control.component.scss']
 })
 export class ControlComponent implements OnInit {
-
+public claseNew?:string;
   constructor() { }
 
   ngOnInit(): void {
   }
+   navigateToSection(section: string) {
+    window.location.hash = '';
+    window.location.hash = section;
+    this.claseNew='classNew';
+}
 
 }
