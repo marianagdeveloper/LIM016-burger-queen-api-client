321c902b931f340953da9e8b7298894a86aa55c3
unknown <fazahuanchef@gmail.com>
2022-02-13 18:50:45 -0500

    add send function with post method in services

--- coffee-queen/src/app/modules/orders/order-list/order-list.metadata.ts
+++ coffee-queen/src/app/modules/orders/order-list/order-list.metadata.ts
@@ -1,7 +1,7 @@
 import { Product } from '../../../shared/components/card/card-product/card-product.metadata';
 
 export interface Order {
-  _id: number,
+  id: number,
   userName: string,
   client: string,
   products: Product[],
