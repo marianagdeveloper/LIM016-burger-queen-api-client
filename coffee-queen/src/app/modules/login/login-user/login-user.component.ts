f289bf398d43ce1997fa2ab0e64f1e679c436f4b
Milagros LC <evidal0710@ich.edu.pe>
2022-02-25 16:06:14 -0500

    void

--- coffee-queen/src/app/modules/login/login-user/login-user.component.ts
+++ coffee-queen/src/app/modules/login/login-user/login-user.component.ts
@@ -9,7 +9,6 @@ import {
 import { Router } from '@angular/router';
 import { UsersService } from 'src/app/data/services/api/users.service';
 
-
 @Component({
   selector: 'app-login-user',
   templateUrl: './login-user.component.html',
@@ -29,9 +28,7 @@ export class LoginUserComponent implements OnInit {
     avatar: ''
   };
   public loginForm!: FormGroup;
-
-  public isCheck: any;
-  public isGetUser: any;
+  public dataUser: any;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -46,9 +43,31 @@ export class LoginUserComponent implements OnInit {
     });
   }
 
-  getUserCredentials():any {
-    this.isCheck = { user: 'checked'}
+  getDataUser(userValidate:any) {
+    this.userService.getAllUsers().subscribe((res) => {
+      let rol = res.filter((a: any) => {
+        if (a.email == userValidate) {
+          this.userData.name = a.name;
+          this.userData.email = a.email;
+          this.userData.roles = a.roles;
+          this.userData.avatar = a.avatar;
+          return a.roles;
+        }
+      });
+      if (rol[0].roles.admin) {
+        this.router.navigate(['product']);
+      } else if (rol[0].roles.cook) {
+        this.router.navigate(['cook-control']);
+      } else if (rol[0].roles.waiter) {
+        this.router.navigate(['product']);
+      }
+      this.userService.disparador.next(this.userData);
 
+    });
+
+  }
+
+  getUserCredentials() {
     this.userService.getAllUsers().subscribe((res) => {
       let userValidate: any;
       const credentials = res.find((a: any) => {
@@ -61,37 +80,12 @@ export class LoginUserComponent implements OnInit {
       });
 
       if (credentials) {
-          let rol = res.filter((a: any) => {
-            if (a.email == userValidate) {
-              this.userData.name = a.name;
-              this.userData.email = a.email;
-              this.userData.roles = a.roles;
-              this.userData.avatar = a.avatar;
-              return a.roles;
-            }
-          });
-
-            if (rol[0].roles.admin) {
-              this.router.navigate(['product']);
-            } else if (rol[0].roles.cook) {
-              this.router.navigate(['cook-control']);
-            } else if (rol[0].roles.waiter) {
-              this.router.navigate(['product']);
-            }
-            this.userService.disparador.next(this.userData);
+        this.getDataUser(userValidate);
 
       } else {
         this.htmlStr = '*Usuario y/o contraseña inválidos.';
       }
-
-      this.isGetUser = this.userData;
-      console.log('return this.isGetUser', this.isGetUser);
-      return this.isGetUser
-
-
-   });
-
-
+    });
   }
 
   campoEsValido(inputForm: string) {
@@ -109,4 +103,16 @@ export class LoginUserComponent implements OnInit {
     this.htmlStr = '';
   }
 
+  /*public users: ILoginUser = {
+    email: '',
+    password: '',
+  };
+  constructor(public loginService: LoginService) {}
+
+  login() {
+    const user = { email: this.users?.email, password: this.users?.password };
+    this.loginService.login(user).subscribe((data) => {
+      console.log(data);
+    });
+  } */
 }
